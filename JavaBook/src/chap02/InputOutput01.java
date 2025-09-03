efde9679a8f6372f65d55e1c42c3ce151351ee1d
user <blossom41902@gmail.com>
2025-09-03 14:37:39 +0900

    추가

---
+++ JavaBook/src/chap02/InputOutput01.java
@@ -0,0 +1,16 @@
+package chap02;
+
+import javax.swing.text.html.HTMLEditorKit.Parser;
+
+public class InputOutput01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "홍길동";
+		int myAge = 20;
+
+		System.out.println("이름 : " + myName);
+		System.out.println("나이 : " + myAge);
+	}
+
+}
